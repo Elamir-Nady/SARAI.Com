200c33d0390a1767c357181983de66ff5adae8d1
Elamir-Nady <ElamirNady@gmail.com>
2021-12-09 16:37:07 +0200

    second

--- src/assets/js/active.js
+++ src/assets/js/active.js
@@ -35,7 +35,6 @@
     // :: 2.2 Mobile Nav Active Code
     var amadoMobNav = $('.amado-navbar-toggler');
     var navClose = $('.nav-close');
-
     amadoMobNav.on('click', function () {
         $('.header-area').toggleClass('bp-xs-on');
     });
